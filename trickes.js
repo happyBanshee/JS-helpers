ad8ad12a5f0b6c49524d1abcc55f087dba703c6d
happyBanshee <alter.koval.olga@gmail.com>
2015-05-17 18:42:02 +0200

    cookies added

--- trickes.js
+++ trickes.js
@@ -16,3 +16,32 @@ mmcore.waitForElementArrival(el).done(function () {
 });
 
 
+//set cookie
+mmcore.SetCookie('cookie_name', 1, 1, 1);
+// set session cookie
+mmcore.SetCookie('cookie_name', 1, 0, 1);
+// set endless cookie
+mmcore.SetCookie('cookie_name', 1, 365, 1);
+// get cookie
+mmcore.GetCookie('cookie_name', 1);
+//remove cookie
+mmcore.SetCookie('mm_payment_passed', -1, -1, 1);
+
+
+// developer helper
+function trackAction(name, val, attr) {
+    //mmcore.$Action(name, val, attr);
+    mmcore.SetAction(name, val, attr);
+    mmcore.request();
+
+    if (navigator.userAgent.toLowerCase().indexOf('msie') + 1) { //for IE<11
+        console.log('Action Tracked: ' + name + ', ' + 1 + (attr ? (', ' + attr) : ''));
+    } else {
+        console.groupCollapsed('%cAction Track: ' + name, 'color:#009922');
+        console.log('%cName: ' + name, 'color:#009922');
+        console.log('%cValue: ' + val, 'color:#009922');
+        console.log('%cAttribute: ' + attr, 'color:#009922');
+        console.groupEnd();
+    }
+}
+
